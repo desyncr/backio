7b17487979a2563a754abf3674207a090c1f8d88
Asphyxia <dario.cavuotti@gmail.com>
2013-06-01 18:58:12 -0300

    Initial commit

---
+++ web/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Asphxia\Batchio\Syncr;
+
+$app = new Silex\Application();
+$configuration = array(
+    'hostname' => 'localhost',
+    'username' => 'example',
+    'password' => 'example',
+    'database' => 'batchio',
+    'max_attemps' => 0,
+    'attemps_delay' => 0
+);
+$app->get('/twilio/callback.me', function (Request $request) use ($configuration) {    
+
+    $syncr = new Syncr\Drivers\Db();
+    $syncr->bootstrap($configuration);
+
+    $status = array(
+        'sid'       => $request->get('SmsSid'),
+        'status'    => $request->get('SmsStatus'),
+        'call_from' => $request->get('From'),
+        'call_to'   => $request->get('To'),
+        'account_sid' => $request->get('AccountSid')
+    );
+
+    $syncr->sync($status);
+    
+    return new Response('', 200);
+});
+
+$app->run();
